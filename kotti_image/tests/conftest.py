648b19be30931d597748ec75b79c389d2892ff27
Andreas Kaiser <disko@binary-punks.com>
2018-03-16 16:23:48 +0100

    Fix setup and tests for Kotti 1.3 / Python 2.7 **and** Kotti 2 / Python 3.x

--- kotti_image/tests/conftest.py
+++ kotti_image/tests/conftest.py
@@ -12,8 +12,13 @@ from pytest import fixture
 
 @fixture(scope='session')
 def custom_settings():
-    # noinspection PyUnresolvedReferences c
-    from kotti_image.resources import Image
-    return {
-        'kotti.configurators': 'kotti_image.kotti_configure'
-    }
+    # Compatibility for Kotti 1.3 and 2.0
+    try:
+        from kotti.resources import Image
+        return {}
+    except ImportError:
+        # noinspection PyUnresolvedReferences c
+        from kotti_image.resources import Image
+        return {
+            'kotti.configurators': 'kotti_image.kotti_configure'
+        }
